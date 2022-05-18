aa983c3752c8f3cbbccd54523353c35c1312ff92
HallexCosta <hallex.costa@hotmail.com>
2022-05-17 22:39:58 -0300

    feat: add project

---
+++ cluster-api/src/index.js
@@ -0,0 +1,24 @@
+import os from 'os'
+import cluster from 'cluster'
+
+const runPrimaryProcess = () => {
+  const processesCount = os.cpus().length * 2
+  console.log(`Primary ${process.pid} is running`)
+  console.log(`Forking server with ${processesCount} process\n`)
+
+  for (let index = 0; index < processesCount; index++)
+    cluster.fork()
+
+  cluster.on('exit', (worker, code, signal) => {
+    if (code !== 0 && !worker.exitedAfterDisconnect) {
+      console.log(`Worker ${worker.process.pid} died... scheduling another one!`)
+      cluster.fork()
+    }
+  })
+}
+
+const runWorkerProcess = async () => {
+  await import('./server.js')
+}
+
+cluster.isPrimary ? runPrimaryProcess() : runWorkerProcess()
---
+++ cluster-api/src/server.js
@@ -0,0 +1,26 @@
+import http from 'node:http'
+
+const processId = process.pid
+
+const server = http.createServer((request, response) => {
+  for (let index = 0; index <= 1e7; index++);
+  response.end(`handled by pid: ${processId}`)
+})
+
+server
+  .listen(3333)
+  .once('listening', () => {
+    console.log('server started in process', processId)
+  })
+
+
+// aguardar as conexões serem encerradas para só então encerrar o programa
+process.on('SIGTERM', () => {
+  console.log('server ending', new Date().toISOString())
+  server.close(() => process.exit())
+})
+
+// vamos simular que um erro aleatorio aconteceu
+setTimeout(() => {
+  process.exit(1)
+}, Math.random() * 1e4)// 10.000
